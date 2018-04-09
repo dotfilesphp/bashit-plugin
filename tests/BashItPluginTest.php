9f9972616c92985b60059ece530c52955be3f5cb
Anthonius Munthi <me@itstoni.com>
2018-04-09 22:38:32 +0800

    cs fix

--- tests/BashItPluginTest.php
+++ tests/BashItPluginTest.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the dotfiles project.
  *
- * (c) Anthonius Munthi <me@itstoni.com>
+ *     (c) Anthonius Munthi <me@itstoni.com>
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was disstributed with this source code.
+ * file that was distributed with this source code.
  */
 
 namespace Dotfiles\Plugins\BashIt\Tests;
@@ -17,16 +19,16 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class BashItPluginTest extends BaseTestCase
 {
-    public function testPlugin()
+    public function testPlugin(): void
     {
         $plugin = new BashItPlugin();
-        $this->assertEquals('bashit',$plugin->getName());
+        $this->assertEquals('bashit', $plugin->getName());
 
         $builder = $this->createMock(ContainerBuilder::class);
         $builder->expects($this->once())
             ->method('fileExists')
             ->with($this->stringContains('services.yaml'))
         ;
-        $plugin->load(array(),$builder);
+        $plugin->load(array(), $builder);
     }
 }
--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the dotfiles project.
  *
- * (c) Anthonius Munthi <me@itstoni.com>
+ *     (c) Anthonius Munthi <me@itstoni.com>
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was disstributed with this source code.
+ * file that was distributed with this source code.
  */
 
 namespace Dotfiles\Plugins\BashIt\Tests;
@@ -17,12 +19,12 @@ use Symfony\Component\Config\Definition\Processor;
 
 class ConfigurationTest extends BaseTestCase
 {
-    public function testTreeBuilder()
+    public function testTreeBuilder(): void
     {
         $processor = new Processor();
-        $config = $processor->processConfiguration(new Configuration(),array());
+        $config = $processor->processConfiguration(new Configuration(), array());
 
-        $this->assertEquals('git@git.domain.com',$config['git_hosting']);
-        $this->assertEquals('atomic',$config['theme_name']);
+        $this->assertEquals('git@git.domain.com', $config['git_hosting']);
+        $this->assertEquals('atomic', $config['theme_name']);
     }
 }
